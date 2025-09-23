fc090d2555a4cb5204c5592ef48c1349a5af98b6
athulkrishnabs <athulrishnabs@gmail.com>
2025-09-23 18:20:47 +0530

    Final dev2 branch commiting

--- Day2/day2.py
+++ Day2/day2.py
@@ -3,3 +3,5 @@ print("Hey,There Github")
 print("So now we have created a branch, All Changes are Made In The Branch")
 
 print("Now This Changes are made in Branch dev2")
+
+# This branch is staying independent. :)
