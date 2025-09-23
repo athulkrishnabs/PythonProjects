165563ead756db20fb3d62bc4c392d3c8b599d73
athulkrishnabs <athulrishnabs@gmail.com>
2025-09-23 14:51:53 +0530

    Day2 Folder Deleted

--- Day2/Day2-Git.py
+++
@@ -1,5 +0,0 @@
-print ("1. #This is a practice to learn the basic GitHub Commands.") 
-
-#well Unknown Errors Have occured
-
-print("These Changes are Made in the dev branch. Useful feature of Github")
