49938c57c1efa38192060eb5b3384792fdd4150a
athulkrishnabs <athulrishnabs@gmail.com>
2025-09-23 15:28:31 +0530

    changes have been made to day2.py in branch dev

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,2 +1,5 @@
 print("This Is day 2 of my Learning AI Automation and Todays Classes are focused On GitHub Basics")
 
+1. # Now we have created a Branch and making our changes in It.
+
+print("So Welcome to the Branch")
