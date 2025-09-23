27ff5a184c3f790c65d55abc14c32bb673e6920b
athulkrishnabs <athulrishnabs@gmail.com>
2025-09-23 14:22:46 +0530

    Made changes in Branch Mode

--- Day2/Day2-Git.py
+++ Day2/Day2-Git.py
@@ -1,3 +1,5 @@
 print ("1. #This is a practice to learn the basic GitHub Commands.") 
 
 #well Unknown Errors Have occured
+
+print("These Changes are Made in the dev branch. Useful feature of Github")
