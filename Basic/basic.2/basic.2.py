27a71044cca7d91c57c1a0ec47ac22ba0a07d055
athulkrishnabs <athulrishnabs@gmail.com>
2025-09-27 12:43:12 +0530

    Task 1

--- Basic/basic.2/basic.2.py
+++ Basic/basic.2/basic.2.py
@@ -1,15 +1,20 @@
-#Write a function to find the length of a string without using len().
+#Function that classifies a score into grades
 
-strings = input("Please Enter Here: ")
-count = 0
+def classifier(n):
+    if n >= 90:
+      return (f"Hey You Have Scored {n}marks and Your Grade is: A")
+    elif n >= 70:
+      return (f"Hey You Have Scored {n}marks and Your Grade is: B") 
+    elif n >= 50:
+      return (f"Hey You Have Scored {n}marks and Your Grade is: C")
+    else:
+      return (f"Hey You Have Scored {n}marks and Your Grade is: F") 
 
-def string_length():
- for string in strings:
-  count += 1
 
-
-print(string_length(strings))
-
-
-
-  
+while True:       
+   try:
+      marks = int(input("Please Enter Your Grades Here: "))
+      print(classifier(marks))
+      break 
+   except ValueError:
+      print("Invalid input Please enter only numbers")
